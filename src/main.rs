7d9b54eba9812a8d431dcc8ce521f5b0f08a0552
ggiunta03 <giunta03@gmail.com>
2024-10-01 07:46:12 -0400

    catching up on git

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+use zero2prod::startup::run;
+use zero2prod::configuration::get_configuration;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    // Read from configuration.yaml file
+    let config = get_configuration().expect("Failed to read config file");
+    // Use localhost and stamped in config port
+    let address = format!("127.0.0.1:{}", config.application_port);
+    let listener = TcpListener::bind(address)?;
+    run(listener)?.await
 }
